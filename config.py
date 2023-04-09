6ca16aa6ae78ebad2d55b1a2f4886a449c268c8a
Uday <udaykumar.1997@gmail.com>
2023-04-09 14:01:54 -0400

    .

---
+++ config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[11]:
+
+
+companies_of_interest = ["Twitter", "Ebix", "Company3","Loki"]
+
+
+# In[12]:
+
+
+# API keys
+TWITTER_API_KEY = "your_twitter_api_key"
+TWITTER_API_SECRET = "your_twitter_api_secret"
+REDDIT_CLIENT_ID = "your_reddit_client_id"
+REDDIT_CLIENT_SECRET = "your_reddit_client_secret"
+
+
+# In[13]:
+
+
+# Database connection settings
+DATABASE_URI = "your_database_uri"
+
+# Other settings
+SCRAPING_INTERVAL = 3600  # Time between scrapes in seconds
+

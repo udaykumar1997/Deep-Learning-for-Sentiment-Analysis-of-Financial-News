a5a9841ac21aea05c3c95c72fe21d160d265adb0
Uday <udaykumar.1997@gmail.com>
2023-05-07 17:28:33 -0400

    .

--- config.py
+++ config.py
@@ -4,25 +4,4 @@
 # In[11]:
 
 
-companies_of_interest = ["Twitter", "Ebix", "Company3","Loki","Blockchain"]
-
-
-# In[12]:
-
-
-# API keys
-TWITTER_API_KEY = "your_twitter_api_key"
-TWITTER_API_SECRET = "your_twitter_api_secret"
-REDDIT_CLIENT_ID = "your_reddit_client_id"
-REDDIT_CLIENT_SECRET = "your_reddit_client_secret"
-
-
-# In[13]:
-
-
-# Database connection settings
-DATABASE_URI = "your_database_uri"
-
-# Other settings
-SCRAPING_INTERVAL = 3600  # Time between scrapes in seconds
-
+companies_of_interest = ["Twitter", "Ebix", "Block","Loki","Blockchain","Nikola", "Adani", "Block", "Kandi", "Lordstown"]
